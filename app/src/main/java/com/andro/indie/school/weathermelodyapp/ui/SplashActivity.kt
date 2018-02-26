24048106a3f859abf4728d50d60d395e90491b16
herisulistiyanto <kid_brotherhood@yahoo.com>
2018-02-26 23:01:02 +0700

    [HERI] initial commit

---
+++ app/src/main/java/com/andro/indie/school/weathermelodyapp/ui/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.andro.indie.school.weathermelodyapp.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.andro.indie.school.weathermelodyapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
